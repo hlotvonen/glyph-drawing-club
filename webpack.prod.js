2bf84b21d4614b7b15aeab0e3c04e92843a482f2
Heikki Lotvonen <hlotvonen@gmail.com>
2019-01-17 13:59:11 +0200

    added separate production and development webpack configs

---
+++ webpack.prod.js
@@ -0,0 +1,65 @@
+var path = require('path');
+var webpack = require('webpack');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  devtool: '',
+  entry: [
+    './src/index'
+  ],
+  optimization: {
+      runtimeChunk: true,
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      },
+      minimizer: [
+        new UglifyJSPlugin({
+            uglifyOptions: {
+              beautify: false,
+              compress: true,
+              ecma: 6,
+              output: {
+                comments: false
+              },
+              compress: {
+                dead_code: true
+              }
+            }
+        })]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: ['babel-loader'],
+      include: path.join(__dirname, 'src')
+    }]
+  },
+  devServer: {
+    publicPath: '/dist/',
+    contentBase: path.resolve(__dirname, ""),
+    watchContentBase: true,
+    compress: true,
+    port: 9001
+  },
+};
