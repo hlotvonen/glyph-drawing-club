c0109f541e4ade14b0858d2db779f334fceeca9c
Heikki Lotvonen <hlotvonen@gmail.com>
2020-02-17 15:47:06 +0200

    added new riso palettes

--- src/components/ColorPresetSelect.js
+++ src/components/ColorPresetSelect.js
@@ -9,11 +9,14 @@ class ColorPaletteSelect extends React.Component {
 			<div className="select_set">
 				Replace with:
 				<select
-				value={colorstore.selectedPresetPalette}
-				onBlur={action(() => colorstore.selectedPresetPalette = '')}
-				onChange={e => colorstore.handlePresetSelectChange(e.target.value)}
+					value={colorstore.selectedPresetPalette}
+					onBlur={action(() => (colorstore.selectedPresetPalette = ""))}
+					onChange={e => colorstore.handlePresetSelectChange(e.target.value)}
 				>
-					<option disabled defaultValue value=""> -- select a preset -- </option>
+					<option disabled defaultValue value="">
+						{" "}
+						-- select a preset --{" "}
+					</option>
 					<option value="0">Deluxe Paint</option>
 					<option value="1">AtariST (Reduced To 256)</option>
 					<option value="2">Eclipse icon 256</option>
@@ -21,8 +24,17 @@ class ColorPaletteSelect extends React.Component {
 					<option value="4">DOOM</option>
 					<option value="5">Riso Blue, Yellow, FluoroPink (Preview)</option>
 					<option value="6">Riso Blue, Yellow, FluoroPink (Blue Plate)</option>
-					<option value="7">Riso Blue, Yellow, FluoroPink (FluoroPink Plate)</option>
-					<option value="8">Riso Blue, Yellow, FluoroPink (Yellow Plate)</option>
+					<option value="7">
+						Riso Blue, Yellow, FluoroPink (FluoroPink Plate)
+					</option>
+					<option value="8">
+						Riso Blue, Yellow, FluoroPink (Yellow Plate)
+					</option>
+					<option value="9">Riso FluoroPink and Teal (Preview)</option>
+					<option value="10">
+						Riso FluoroPink and Teal (FluoroPink Plate)
+					</option>
+					<option value="11">Riso FluoroPink and Teal (Teal Plate)</option>
 				</select>
 			</div>
 		)
